91e4f9ddefbdd97d02b2ffd27d749b541b101e1f
slamatik <vjaceslavsmatess@gmail.com>
2020-07-31 19:48:49 +0100

    Solution

--- Dynamic Programming/Removing Digits.py
+++ Dynamic Programming/Removing Digits.py
@@ -1,25 +1,6 @@
-from sys import maxsize as inf
-
-dp = {0: 0}
-
-
-# def remove_digits(n):
-#     for i in range(1, n+1):
-#         temp = i
-#         min_steps = inf
-#         while temp != 0:
-#             di = temp % 10
-#             temp //= 10
-#             if di == 0: continue
-#             min_steps = min(min_steps, 1 + dp[i - di])
-#         dp[i] = min_steps
-#     print(dp)
-#
-# remove_digits(123)
 n = int(input())
-count = 0
-while n > 9:
-    num = max([int(i) for i in str(n)])
-    n -= num
-    count += 1
-print(count + 1)
+c = 0
+while n:
+    n -= max([int(i) for i in str(n)])
+    c += 1
+print(c)
